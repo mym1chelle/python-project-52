ebaf237b845a7e8e248b70e33d28f72fb21ef785
Timur <81976283+mym1chelle@users.noreply.github.com>
2022-12-20 11:42:18 +0400

    Setting up static files

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -139,7 +139,8 @@ LANGUAGES = (
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = []
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
