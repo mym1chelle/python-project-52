030f902bde31ff1aaaeeaa366272c15e5500efe1
Timur <81976283+mym1chelle@users.noreply.github.com>
2022-12-26 14:40:16 +0400

    Add new app Labels

---
+++ task_manager/labels/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from task_manager.labels import views
+
+
+app_name = 'labels'
+urlpatterns = [
+
+]
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -38,5 +38,6 @@ urlpatterns += i18n_patterns(
 
     path('users/', include('task_manager.users.urls')),
     path('statuses/', include('task_manager.statuses.urls')),
-    path('tasks/', include('task_manager.tasks.urls'))
+    path('tasks/', include('task_manager.tasks.urls')),
+    path('labels/', include('task_manager.labels.urls'))
 )
